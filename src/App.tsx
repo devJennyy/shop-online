1e99a5be7e26d20a609ce98805431a1787b43443
Jenny Pieloor <jennypieloor.connect@gmail.com>
2024-04-16 14:12:14 +0800

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -415,37 +415,37 @@ function App() {
           {/* Footer */}
           <div className="flex flex-col mt-28 gap-28 w-full">
             <div className="flex flex-row justify-between items-start">
-              <div className="flex flex-col items-center gap-8 w-80">
+              <div className="flex flex-col items-center xl:gap-8 lg:gap-6 xl:w-80 lg:w-72">
                 <button>
                   <img src="/icons/logo.svg" className="w-56" />
                 </button>
-                <p className="w-full text-[14px] text-[#666666] font-normal text-justify">
+                <p className="w-full xl:text-[14px] lg:text-[12px] text-[#666666] font-normal text-justify">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
 
                 <div className="flex flex-row justify-between items-center w-full">
-                  <button className="flex flex-row justify-center items-center w-12 h-12 rounded-2xl bg-[#EDF0F7] active:scale-95 transition-all">
+                  <button className="flex flex-row justify-center items-center xl:w-12 xl:h-12 lg:w-10 lg:h-10 rounded-2xl bg-[#EDF0F7] active:scale-95 transition-all">
                     <FaTwitter size={16} />
                   </button>
-                  <button className="flex flex-row justify-center items-center w-12 h-12 rounded-2xl bg-[#EDF0F7] active:scale-95 transition-all">
+                  <button className="flex flex-row justify-center items-center xl:w-12 xl:h-12 lg:w-10 lg:h-10 rounded-2xl bg-[#EDF0F7] active:scale-95 transition-all">
                     <FaFacebookF size={16} />
                   </button>
-                  <button className="flex flex-row justify-center items-center w-12 h-12 rounded-2xl bg-[#EDF0F7] active:scale-95 transition-all">
+                  <button className="flex flex-row justify-center items-center xl:w-12 xl:h-12 lg:w-10 lg:h-10 rounded-2xl bg-[#EDF0F7] active:scale-95 transition-all">
                     <FaYoutube size={16} />
                   </button>
-                  <button className="flex flex-row justify-center items-center w-12 h-12 rounded-2xl bg-[#EDF0F7] active:scale-95 transition-all">
+                  <button className="flex flex-row justify-center items-center xl:w-12 xl:h-12 lg:w-10 lg:h-10 rounded-2xl bg-[#EDF0F7] active:scale-95 transition-all">
                     <FaInstagram size={16} />
                   </button>
-                  <button className="flex flex-row justify-center items-center w-12 h-12 rounded-2xl bg-[#EDF0F7] active:scale-95 transition-all">
+                  <button className="flex flex-row justify-center items-center xl:w-12 xl:h-12 lg:w-10 lg:h-10 rounded-2xl bg-[#EDF0F7] active:scale-95 transition-all">
                     <FaPinterestP size={16} />
                   </button>
                 </div>
               </div>
 
               <div className="flex flex-col justify-start items-start gap-8">
-                <p className="text-[18px] font-semibold">Company</p>
-                <div className="flex flex-col justify-start items-start text-[#666666] text-[14px] font-normal gap-2">
+                <p className="xl:text-[18px] lg:text-[16px] font-semibold">Company</p>
+                <div className="flex flex-col justify-start items-start text-[#666666] xl:text-[14px] lg:text-[12px] font-normal gap-2">
                   <button className="hover:text-[#030812]">About</button>
                   <button className="hover:text-[#030812]">Contact</button>
                   <button className="hover:text-[#030812]">Sitemap</button>
@@ -456,8 +456,8 @@ function App() {
               </div>
 
               <div className="flex flex-col justify-start items-start gap-8">
-                <p className="text-[18px] font-semibold">Help Center</p>
-                <div className="flex flex-col justify-start items-start text-[#666666] text-[14px] font-normal gap-2">
+                <p className="xl:text-[18px] lg:text-[16px] font-semibold">Help Center</p>
+                <div className="flex flex-col justify-start items-start text-[#666666] xl:text-[14px] lg:text-[12px] font-normal gap-2">
                   <button className="hover:text-[#030812]">
                     Customer Service
                   </button>
@@ -470,8 +470,8 @@ function App() {
               </div>
 
               <div className="flex flex-col justify-start items-start gap-8">
-                <p className="text-[18px] font-semibold">Partner</p>
-                <div className="flex flex-col justify-start items-start text-[#666666] text-[14px] font-normal gap-2">
+                <p className="xl:text-[18px] lg:text-[16px] font-semibold">Partner</p>
+                <div className="flex flex-col justify-start items-start text-[#666666] xl:text-[14px] lg:text-[12px] font-normal gap-2">
                   <button className="hover:text-[#030812]">
                     Become Seller
                   </button>
@@ -482,9 +482,9 @@ function App() {
               </div>
 
               <div className="flex flex-col justify-start items-start gap-8">
-                <p className="text-[18px] font-semibold">Get app</p>
+                <p className="xl:text-[18px] lg:text-[16px] font-semibold">Get app</p>
                 <div className="flex flex-col gap-2">
-                  <button className="flex justify-center items-center w-40 h-12 rounded-md bg-[#1C1C1C] text-white active:scale-95 transition-all">
+                  <button className="flex justify-center items-center xl:w-40 lg:w-36 h-12 rounded-md bg-[#1C1C1C] text-white active:scale-95 transition-all">
                     <div className="flex flex-row justify-center items-center gap-3">
                       <FaApple size={22} />
                       <div className="flex flex-col justify-start items-start">
@@ -494,7 +494,7 @@ function App() {
                     </div>
                   </button>
 
-                  <button className="flex justify-center items-center w-40 h-12 rounded-md bg-[#1C1C1C] text-white active:scale-95 transition-all">
+                  <button className="flex justify-center items-center xl:w-40 lg:w-36 h-12 rounded-md bg-[#1C1C1C] text-white active:scale-95 transition-all">
                     <div className="flex flex-row justify-center items-center gap-3">
                       <FaGooglePlay />
                       <div className="flex flex-col justify-start items-start">
