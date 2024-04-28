5ffb1d20db1ea9b65db6a09c7b57ade01a8fecf2
devJennyy <devjenny.official@gmail.com>
2024-04-28 23:09:35 +0800

    Wip

--- src/Pages/Items/ShopPage/Shop.tsx
+++ src/Pages/Items/ShopPage/Shop.tsx
@@ -13,13 +13,12 @@ const Shop = () => {
           Ultimate Sales Collection!
         </p>
 
-        {/* Filters */}
         <div className="flex flex-row justify-between w-full h-[100vh]">
-          <div className="w-96 h-full">
+          {/* Filters */}
+          <div className="w-[450px] h-full">
             <p className="text-[20px] font-semibold">Filters</p>
 
             <div className="flex flex-col text-start gap-10 mt-5">
-
               <div className="flex flex-col gap-4">
                 <p className="text-[18px] font-medium">Size</p>
 
@@ -43,25 +42,25 @@ const Shop = () => {
                 <p className="text-[18px] font-medium">Colors</p>
 
                 <div className="flex flex-col gap-2">
-                <div className="flex flex-row gap-2">
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                </div>
+                  <div className="flex flex-row gap-2">
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                  </div>
 
-                <div className="flex flex-row gap-2">
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                  <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                </div>
+                  <div className="flex flex-row gap-2">
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
+                  </div>
                 </div>
               </div>
 
@@ -91,7 +90,39 @@ const Shop = () => {
             </div>
           </div>
 
-          <div className="w-full h-full bg-slate-800"></div>
+          {/* Content */}
+          <div className="w-full h-full ">
+            <p>Best Selling</p>
+
+            <div className="flex flex-row mt-5">
+              <button className="flex w-[300px] h-[400px] border border-[#E0E0E0] rounded-md">
+                <div className="flex flex-col justify-center items-center p-4 w-full gap-6 mt-2">
+                  <img
+                    src="/images/mens-apparel.png"
+                    className="flex justify-center items-center object-scale-down w-fit h-fit"
+                  />
+
+                  <div className="flex flex-col justify-center items-start w-full">
+                    <div className="flex flex-row gap-3">
+                      <p className="font-medium">$10.30</p>
+                      
+                    </div>
+                    <p className="text-[14px] text-[#8B96A5]">
+                      T-shirts for Men
+                    </p>
+                    <div className="flex flex-row justify-between w-full mt-6">
+                     <div className="flex flex-row justify-center items-center gap-2">
+                      <div className="w-3 h-3 bg-blue-900 rounded-full outline outline-1 outline-offset-2"></div>
+                      <div className="w-4 h-4 bg-black rounded-full"></div>
+                      <div className="w-4 h-4 bg-yellow-200 rounded-full"></div>
+
+                     </div>
+                    </div>
+                  </div>
+                </div>
+              </button>
+            </div>
+          </div>
         </div>
 
         {/* Footer */}
