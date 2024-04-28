9ddb53b1e05769c57b9a156eb7d99545816563e2
devJennyy <devjenny.official@gmail.com>
2024-04-28 23:43:18 +0800

    Test

--- src/Pages/Items/ShopPage/Shop.tsx
+++ src/Pages/Items/ShopPage/Shop.tsx
@@ -18,7 +18,7 @@ const Shop = () => {
               <div className="flex flex-col gap-4">
                 <p className="text-[18px] font-medium">Size</p>
 
-                <div className="flex flex-row gap-4">
+                <div className="flex flex-row gap-3">
                   <div className="flex justify-center items-center border w-11 h-11 rounded-md">
                     S
                   </div>
@@ -45,11 +45,9 @@ const Shop = () => {
                     <div className="w-7 h-7 rounded-full bg-slate-600"></div>
                     <div className="w-7 h-7 rounded-full bg-slate-600"></div>
                     <div className="w-7 h-7 rounded-full bg-slate-600"></div>
-                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
                   </div>
 
                   <div className="flex flex-row gap-2">
-                    <div className="w-7 h-7 rounded-full bg-slate-600"></div>
                     <div className="w-7 h-7 rounded-full bg-slate-600"></div>
                     <div className="w-7 h-7 rounded-full bg-slate-600"></div>
                     <div className="w-7 h-7 rounded-full bg-slate-600"></div>
@@ -88,10 +86,85 @@ const Shop = () => {
 
           {/* Content */}
           <div className="w-full h-full ">
-            <p>Best Selling</p>
+            <p className="font-medium">Best Selling</p>
 
-            <div className="flex flex-row mt-5 gap-5">
-              <button className="flex w-[300px] h-[400px] border border-[#E0E0E0] rounded-md">
+            <div className="flex flex-col">
+              <div className="flex flex-row mt-5 gap-4">
+                <button className="flex w-[285px] h-[400px] border border-[#E0E0E0] rounded-md">
+                  <div className="flex flex-col justify-center items-center p-4 w-full">
+                    <div className="w-full h-[260px]">
+                      <img
+                        src="/images/shop-item/item-one.png"
+                        className="flex justify-center items-center object-cover w-full h-full"
+                      />
+                    </div>
+
+                    <div className="flex flex-col items-start w-full mt-5">
+                      <div className=" flex flex-col text-start gap-1">
+                        <p className="font-medium">Boxy Denim Hat</p>
+
+                        <p className="text-[14px]">$ 25.00</p>
+                      </div>
+
+                      <div className="flex flex-row justify-center items-center gap-3 mt-4">
+                        <div className="w-4 h-4 bg-[#B1C5D4] rounded-full outline outline-1 outline-offset-2"></div>
+                        <div className="w-5 h-5 bg-[#063E66] rounded-full"></div>
+                      </div>
+                    </div>
+                  </div>
+                </button>
+
+                <button className="flex w-[285px] h-[400px] border border-[#E0E0E0] rounded-md">
+                  <div className="flex flex-col justify-center items-center p-4 w-full">
+                    <div className="w-full h-[260px]">
+                      <img
+                        src="/images/shop-item/item-one.png"
+                        className="flex justify-center items-center object-cover w-full h-full"
+                      />
+                    </div>
+
+                    <div className="flex flex-col items-start w-full mt-5">
+                      <div className=" flex flex-col text-start gap-1">
+                        <p className="font-medium">Boxy Denim Hat</p>
+
+                        <p className="text-[14px]">$ 25.00</p>
+                      </div>
+
+                      <div className="flex flex-row justify-center items-center gap-3 mt-4">
+                        <div className="w-4 h-4 bg-[#B1C5D4] rounded-full outline outline-1 outline-offset-2"></div>
+                        <div className="w-5 h-5 bg-[#063E66] rounded-full"></div>
+                      </div>
+                    </div>
+                  </div>
+                </button>
+
+                <button className="flex w-[285px] h-[400px] border border-[#E0E0E0] rounded-md">
+                  <div className="flex flex-col justify-center items-center p-4 w-full">
+                    <div className="w-full h-[260px]">
+                      <img
+                        src="/images/shop-item/item-one.png"
+                        className="flex justify-center items-center object-cover w-full h-full"
+                      />
+                    </div>
+
+                    <div className="flex flex-col items-start w-full mt-5">
+                      <div className=" flex flex-col text-start gap-1">
+                        <p className="font-medium">Boxy Denim Hat</p>
+
+                        <p className="text-[14px]">$ 25.00</p>
+                      </div>
+
+                      <div className="flex flex-row justify-center items-center gap-3 mt-4">
+                        <div className="w-4 h-4 bg-[#B1C5D4] rounded-full outline outline-1 outline-offset-2"></div>
+                        <div className="w-5 h-5 bg-[#063E66] rounded-full"></div>
+                      </div>
+                    </div>
+                  </div>
+                </button>
+              </div>
+
+              <div className="flex flex-row mt-5 gap-4">
+              <button className="flex w-[285px] h-[400px] border border-[#E0E0E0] rounded-md">
                 <div className="flex flex-col justify-center items-center p-4 w-full">
                   <div className="w-full h-[260px]">
                     <img
@@ -115,7 +188,31 @@ const Shop = () => {
                 </div>
               </button>
 
-              <button className="flex w-[300px] h-[400px] border border-[#E0E0E0] rounded-md">
+              <button className="flex w-[285px] h-[400px] border border-[#E0E0E0] rounded-md">
+                <div className="flex flex-col justify-center items-center p-4 w-full">
+                  <div className="w-full h-[260px]">
+                    <img
+                      src="/images/shop-item/item-one.png"
+                      className="flex justify-center items-center object-cover w-full h-full"
+                    />
+                  </div>
+
+                  <div className="flex flex-col items-start w-full mt-5">
+                    <div className=" flex flex-col text-start gap-1">
+                      <p className="font-medium">Boxy Denim Hat</p>
+
+                      <p className="text-[14px]">$ 25.00</p>
+                    </div>
+
+                    <div className="flex flex-row justify-center items-center gap-3 mt-4">
+                      <div className="w-4 h-4 bg-[#B1C5D4] rounded-full outline outline-1 outline-offset-2"></div>
+                      <div className="w-5 h-5 bg-[#063E66] rounded-full"></div>
+                    </div>
+                  </div>
+                </div>
+              </button>
+
+              <button className="flex w-[285px] h-[400px] border border-[#E0E0E0] rounded-md">
                 <div className="flex flex-col justify-center items-center p-4 w-full">
                   <div className="w-full h-[260px]">
                     <img
@@ -139,8 +236,7 @@ const Shop = () => {
                 </div>
               </button>
             </div>
-
-            
+            </div>
           </div>
         </div>
 
