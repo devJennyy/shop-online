2c61ba837f66a34eeb17ba7744959e3f272f18bd
Jenny Pieloor <devjenny.official@gmail.com>
2024-08-01 22:19:02 +0800

    Updated discovery data

--- src/components/cards/Discovery.tsx
+++ src/components/cards/Discovery.tsx
@@ -1,3 +1,4 @@
+import { NumericFormat } from "react-number-format";
 interface Props {
   itemImage: string;
   currentPrice: number;
@@ -19,17 +20,30 @@ const Discovery = ({
     <button className="flex w-56 h-80 border border-[#E0E0E0] rounded-md">
       <div className="flex flex-col justify-center items-center p-4 w-full gap-6 mt-2">
         <div className="flex justify-center items-center w-36 h-[10.7rem] object-scale-down">
-          <img
-            src={itemImage}
-            className="w-full h-full"
-          />
+          <img src={itemImage} className="w-full h-full" />
         </div>
 
         <div className="flex flex-col justify-center items-start w-full">
           <div className="flex flex-row gap-3">
-            <p className="font-medium">${currentPrice}</p>
+            <p className="font-medium">
+              <NumericFormat
+                displayType="text"
+                value={currentPrice}
+                thousandSeparator=","
+                decimalScale={2}
+                fixedDecimalScale={true}
+                prefix="$"
+              />
+            </p>
             <p className="line-through text-[#666666] text-[12px] font-semibold">
-              ${previousPrice}
+              <NumericFormat
+                displayType="text"
+                value={previousPrice}
+                thousandSeparator=","
+                decimalScale={2}
+                fixedDecimalScale={true}
+                prefix="$"
+              />
             </p>
           </div>
           <p className="text-[14px] text-[#8B96A5]">{itemName}</p>
