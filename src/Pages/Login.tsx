84e37e0fb306d70d279a8dddc712dea6fe1531d9
Jenny Pieloor <devjenny.official@gmail.com>
2024-09-17 05:25:16 +0800

    Updated Signup and Login page layouts.

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -11,129 +11,118 @@ const Login = () => {
   return (
     <div className="w-full">
       <Header hasNavbar={false} />
-
-      <div className="flex flex-col justify-center items-center w-[1240px] mx-auto h-full px-8 text-richBlack pt-28 gap-10">
+      <main className="flex flex-col justify-center items-center w-[1240px] mx-auto h-full px-8 text-richBlack pt-28 gap-10">
         <div className="flex justify-between w-full gap-5">
-          <div className="flex w-1/2 h-full">
-            <div className="flex flex-col gap-10 mt-20">
-              <CardContainer className="inter-var">
-                <CardItem translateZ="100">
-                  <img
-                    src="/images/plusone/image-three.jpg"
-                    className="object-cover w-60 h-60 rounded-[50px] "
-                  />
-                </CardItem>
-              </CardContainer>
-
-              <CardContainer className="inter-var">
-                <CardItem translateZ="100">
-                  <img
-                    src="/images/plusone/image-two.jpg"
-                    className="object-cover object-center w-60 h-72 rounded-[50px] ml-10"
-                  />
-                </CardItem>
-              </CardContainer>
-            </div>
-
-            <div className="flex flex-col gap-10">
-              <CardContainer className="inter-var">
-                <CardItem translateZ="100">
-                  <img
-                    src="/images/plusone/image-one.gif"
-                    className="object-cover w-60 h-72 rounded-[50px]"
-                  />
-                </CardItem>
-              </CardContainer>
-
-              <CardContainer className="inter-var">
-                <CardItem translateZ="100">
-                  <img
-                    src="/images/plusone/image-four.jpg"
-                    className="object-cover w-60 h-60 rounded-[50px] ml-10"
-                  />
-                </CardItem>
-              </CardContainer>
-            </div>
-          </div>
-
-          <div className="w-[500px] border border-gray-lightGray rounded-3xl p-10">
-            <p className="text-[26px] font-semibold">Welcome Back!</p>
-            <p className="text-[14px] text-gray-darkGray">
-              Please enter your details.
-            </p>
-
-            <div className="flex flex-col w-full gap-4 mt-10 text-[14px]">
-              <div className="flex flex-col w-full gap-1">
-                <p className="text-gray-darkGray">
-                  Phone number, user name, or email address
-                </p>
-                <input className="w-full h-11 text-[14px] font-normal border border-gray-lightGray rounded-xl pl-4 pr-12 outline-none focus:shadow-sm focus-visible:ring-1 ring-stone-100" />
-              </div>
-
-              <div className="flex flex-col w-full gap-1">
-                <div className="flex justify-between text-gray-darkGray">
-                  <p>Password</p>
-                  <button className="flex justify-center items-center gap-2">
-                    <BiSolidHide size={20} className="text-gray-darkGray/80" />
-                    <p>Hide</p>
-                  </button>
-                </div>
-
-                <input className="w-full h-11 text-[14px] font-normal border border-gray-lightGray rounded-xl pl-4 pr-12 outline-none focus:shadow-sm focus-visible:ring-1 ring-stone-100" />
-                <button className="flex justify-end text-sm underline mt-1">
-                  Forgot Password
-                </button>
-
-                <div className="flex items-center space-x-2">
-                  <Checkbox id="terms" />
-                  <label
-                    htmlFor="terms"
-                    className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-richBlack"
-                  >
-                    Remember me
-                  </label>
-                </div>
-              </div>
-
-              <button className="flex justify-center items-center w-full h-12 bg-richBlack rounded-xl mt-2 active:scale-95 transition-all">
-                <p className="text-[16px] text-white font-medium">Log in</p>
-              </button>
-
-              <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-4 h-[1px] w-full"></div>
-
-              <div className="flex flex-col gap-2">
-                <button className="flex justify-center items-center w-full h-11 border border-gray-lightGray rounded-full gap-2">
-                  <FcGoogle size={20} />
-                  <p>Continue with Google</p>
-                </button>
-                <button className="flex justify-center items-center w-full h-11 border border-gray-lightGray rounded-full gap-2">
-                  <FaFacebook size={20} className="text-blue-600" />
-                  <p>Continue with Facebook</p>
-                </button>
-              </div>
-
-              <div className="flex justify-center items-center gap-2 mt-5">
-                <p className="text-gray-darkGray">
-                  New to <span>ShopOnline?</span>
-                </p>
-                <Link
-                  to={"/signup"}
-                  className="font-semibold underline underline-offset-2"
-                >
-                  Sign up
-                </Link>
-              </div>
-            </div>
-          </div>
+          <ImageGallery />
+          <LoginForm />
         </div>
-
-        {/* Footer */}
-        <div className="w-full">
-          <Footer hasMiniHighlights={false} hasBorder={true} />
-        </div>
-      </div>
+        <FooterSection />
+      </main>
     </div>
   );
 };
+
+const ImageGallery = () => (
+  <div className="flex w-1/2 h-full">
+    <div className="flex flex-col gap-10 mt-20">
+      <ImageCard src="/images/plusone/image-three.jpg" className="w-60 h-60" />
+      <ImageCard src="/images/plusone/image-two.jpg" className="w-60 h-72 ml-10" />
+    </div>
+    <div className="flex flex-col gap-10">
+      <ImageCard src="/images/plusone/image-one.gif" className="w-60 h-72" />
+      <ImageCard src="/images/plusone/image-four.jpg" className="w-60 h-60 ml-10" />
+    </div>
+  </div>
+);
+
+const ImageCard = ({ src, className }: { src: string; className: string }) => (
+  <CardContainer className="inter-var">
+    <CardItem translateZ="100">
+      <img src={src} className={`object-cover rounded-[50px] ${className}`} />
+    </CardItem>
+  </CardContainer>
+);
+
+const LoginForm = () => (
+  <div className="w-[500px] border border-gray-lightGray rounded-3xl p-10">
+    <p className="text-[26px] font-semibold">Welcome Back!</p>
+    <p className="text-[14px] text-gray-darkGray">Please enter your details.</p>
+
+    <div className="flex flex-col w-full gap-4 mt-10 text-[14px]">
+      <TextInput label="Phone number, user name, or email address" />
+      <PasswordInput />
+      <RememberMeCheckbox />
+      <LoginButton />
+      <SocialLoginOptions />
+      <SignupPrompt />
+    </div>
+  </div>
+);
+
+const TextInput = ({ label }: { label: string }) => (
+  <div className="flex flex-col w-full gap-1">
+    <p className="text-gray-darkGray">{label}</p>
+    <input className="w-full h-11 text-[14px] font-normal border border-gray-lightGray rounded-xl pl-4 pr-12 outline-none focus:shadow-sm focus-visible:ring-1 ring-stone-100" />
+  </div>
+);
+
+const PasswordInput = () => (
+  <div className="flex flex-col w-full gap-1">
+    <div className="flex justify-between text-gray-darkGray">
+      <p>Password</p>
+      <button className="flex justify-center items-center gap-2">
+        <BiSolidHide size={20} className="text-gray-darkGray/80" />
+        <p>Hide</p>
+      </button>
+    </div>
+    <input className="w-full h-11 text-[14px] font-normal border border-gray-lightGray rounded-xl pl-4 pr-12 outline-none focus:shadow-sm focus-visible:ring-1 ring-stone-100" />
+    <button className="flex justify-end text-sm underline mt-1">Forgot Password</button>
+  </div>
+);
+
+const RememberMeCheckbox = () => (
+  <div className="flex items-center space-x-2">
+    <Checkbox id="terms" />
+    <label htmlFor="terms" className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-richBlack">
+      Remember me
+    </label>
+  </div>
+);
+
+const LoginButton = () => (
+  <button className="flex justify-center items-center w-full h-12 bg-richBlack rounded-xl mt-2 active:scale-95 transition-all">
+    <p className="text-[16px] text-white font-medium">Log in</p>
+  </button>
+);
+
+const SocialLoginOptions = () => (
+  <>
+    <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-4 h-[1px] w-full"></div>
+    <div className="flex flex-col gap-2">
+      <SocialButton icon={<FcGoogle size={20} />} text="Continue with Google" />
+      <SocialButton icon={<FaFacebook size={20} className="text-blue-600" />} text="Continue with Facebook" />
+    </div>
+  </>
+);
+
+const SocialButton = ({ icon, text }: { icon: React.ReactNode; text: string }) => (
+  <button className="flex justify-center items-center w-full h-11 border border-gray-lightGray rounded-full gap-2">
+    {icon}
+    <p>{text}</p>
+  </button>
+);
+
+const SignupPrompt = () => (
+  <div className="flex justify-center items-center gap-2 mt-5">
+    <p className="text-gray-darkGray">New to <span>ShopOnline?</span></p>
+    <Link to={"/signup"} className="font-semibold underline underline-offset-2">Sign up</Link>
+  </div>
+);
+
+const FooterSection = () => (
+  <div className="w-full">
+    <Footer hasMiniHighlights={false} hasBorder={true} />
+  </div>
+);
 
 export default Login;
--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -10,124 +10,111 @@ const Signup = () => {
   return (
     <div className="w-full">
       <Header hasNavbar={false} />
-
-      <div className="flex flex-col justify-center items-center w-[1240px] mx-auto h-full px-8 text-richBlack pt-28 gap-10">
+      <main className="flex flex-col justify-center items-center w-[1240px] mx-auto h-full px-8 text-richBlack pt-28 gap-10">
         <div className="flex justify-between w-full gap-5">
-          <div className="flex w-1/2">
-            <div className="flex flex-col gap-10 mt-20">
-              <CardContainer className="inter-var">
-                <CardItem translateZ="100">
-                  <img
-                    src="/images/plusone/image-three.jpg"
-                    className="object-cover w-60 h-60 rounded-[50px] "
-                  />
-                </CardItem>
-              </CardContainer>
-
-              <CardContainer className="inter-var">
-                <CardItem translateZ="100">
-                  <img
-                    src="/images/plusone/image-two.jpg"
-                    className="object-cover object-center w-60 h-72 rounded-[50px] ml-10"
-                  />
-                </CardItem>
-              </CardContainer>
-            </div>
-
-            <div className="flex flex-col gap-10">
-              <CardContainer className="inter-var">
-                <CardItem translateZ="100">
-                  <img
-                    src="/images/plusone/image-one.gif"
-                    className="object-cover w-60 h-72 rounded-[50px]"
-                  />
-                </CardItem>
-              </CardContainer>
-
-              <CardContainer className="inter-var">
-                <CardItem translateZ="100">
-                  <img
-                    src="/images/plusone/image-four.jpg"
-                    className="object-cover w-60 h-60 rounded-[50px] ml-10"
-                  />
-                </CardItem>
-              </CardContainer>
-            </div>
-          </div>
-
-          <div className="w-[500px] border border-gray-lightGray rounded-3xl p-10">
-            <p className="text-[26px] font-semibold">Create an account</p>
-            <p className="text-[14px] text-gray-darkGray">Lorem Ipsum Dolor.</p>
-
-            <div className="flex flex-col w-full gap-4 mt-10 text-[14px]">
-              <div className="flex gap-4">
-                <div className="flex flex-col w-full gap-1">
-                  <p className="text-gray-darkGray">First name</p>
-                  <input className="w-full h-11 text-[14px] font-normal border border-gray-lightGray rounded-xl pl-4 pr-12 outline-none focus:shadow-sm focus-visible:ring-1 ring-stone-100" />
-                </div>
-                <div className="flex flex-col w-full gap-1">
-                  <p className="text-gray-darkGray">Last name</p>
-                  <input className="w-full h-11 text-[14px] font-normal border border-gray-lightGray rounded-xl pl-4 pr-12 outline-none focus:shadow-sm focus-visible:ring-1 ring-stone-100" />
-                </div>
-              </div>
-
-              <div className="flex flex-col w-full gap-1">
-                <p className="text-gray-darkGray">
-                  Email address or phone number
-                </p>
-                <input className="w-full h-11 text-[14px] font-normal border border-gray-lightGray rounded-xl pl-4 pr-12 outline-none focus:shadow-sm focus-visible:ring-1 ring-stone-100" />
-              </div>
-
-              <div className="flex flex-col w-full gap-1">
-                <div className="flex justify-between text-gray-darkGray">
-                  <p>Password</p>
-                  <button className="flex justify-center items-center gap-2">
-                    <BiSolidHide size={20} className="text-gray-darkGray/80" />
-                    <p>Hide</p>
-                  </button>
-                </div>
-
-                <input className="w-full h-11 text-[14px] font-normal border border-gray-lightGray rounded-xl pl-4 pr-12 outline-none focus:shadow-sm focus-visible:ring-1 ring-stone-100" />
-              </div>
-
-              <button className="flex justify-center items-center w-full h-12 bg-richBlack rounded-xl mt-4 active:scale-95 transition-all">
-                <p className="text-[16px] text-white font-medium">
-                  Create account
-                </p>
-              </button>
-
-              <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-4 h-[1px] w-full"></div>
-
-              <div className="flex flex-col gap-2">
-                <button className="flex justify-center items-center w-full h-11 border border-gray-lightGray rounded-full gap-2">
-                  <FcGoogle size={20} />
-                  <p>Continue with Google</p>
-                </button>
-                <button className="flex justify-center items-center w-full h-11 border border-gray-lightGray rounded-full gap-2">
-                  <FaFacebook size={20} className="text-blue-600" />
-                  <p>Continue with Facebook</p>
-                </button>
-              </div>
-
-              <div className="flex justify-center items-center gap-2 mt-5">
-                <p className="text-gray-darkGray">Already have an account?</p>
-                <Link
-                  to={"/login"}
-                  className="font-semibold underline underline-offset-2"
-                >
-                  Log in
-                </Link>
-              </div>
-            </div>
-          </div>
+          <ImageGallery />
+          <SignupForm />
         </div>
-
-        <div className="w-full">
-          <Footer hasMiniHighlights={false} hasBorder={true} />
-        </div>
-      </div>
+        <FooterSection />
+      </main>
     </div>
   );
 };
+
+const ImageGallery = () => (
+  <div className="flex w-1/2 h-full">
+    <div className="flex flex-col gap-10 mt-20">
+      <ImageCard src="/images/plusone/image-three.jpg" className="w-60 h-60" />
+      <ImageCard src="/images/plusone/image-two.jpg" className="w-60 h-72 ml-10" />
+    </div>
+    <div className="flex flex-col gap-10">
+      <ImageCard src="/images/plusone/image-one.gif" className="w-60 h-72" />
+      <ImageCard src="/images/plusone/image-four.jpg" className="w-60 h-60 ml-10" />
+    </div>
+  </div>
+);
+
+const ImageCard = ({ src, className }: { src: string; className: string }) => (
+  <CardContainer className="inter-var">
+    <CardItem translateZ="100">
+      <img src={src} className={`object-cover rounded-[50px] ${className}`} />
+    </CardItem>
+  </CardContainer>
+);
+
+const SignupForm = () => (
+  <div className="w-[500px] border border-gray-lightGray rounded-3xl p-10">
+    <p className="text-[26px] font-semibold">Create an account</p>
+    <p className="text-[14px] text-gray-darkGray">Lorem Ipsum Dolor.</p>
+
+    <div className="flex flex-col w-full gap-4 mt-10 text-[14px]">
+      <div className="flex gap-4">
+        <TextInput label="First name" />
+        <TextInput label="Last name" />
+      </div>
+      <TextInput label="Email address or phone number" />
+      <PasswordInput />
+      <SignupButton />
+      <SocialLoginOptions />
+      <LoginPrompt />
+    </div>
+  </div>
+);
+
+const TextInput = ({ label }: { label: string }) => (
+  <div className="flex flex-col w-full gap-1">
+    <p className="text-gray-darkGray">{label}</p>
+    <input className="w-full h-11 text-[14px] font-normal border border-gray-lightGray rounded-xl pl-4 pr-12 outline-none focus:shadow-sm focus-visible:ring-1 ring-stone-100" />
+  </div>
+);
+
+const PasswordInput = () => (
+  <div className="flex flex-col w-full gap-1">
+    <div className="flex justify-between text-gray-darkGray">
+      <p>Password</p>
+      <button className="flex justify-center items-center gap-2">
+        <BiSolidHide size={20} className="text-gray-darkGray/80" />
+        <p>Hide</p>
+      </button>
+    </div>
+    <input className="w-full h-11 text-[14px] font-normal border border-gray-lightGray rounded-xl pl-4 pr-12 outline-none focus:shadow-sm focus-visible:ring-1 ring-stone-100" />
+  </div>
+);
+
+const SignupButton = () => (
+  <button className="flex justify-center items-center w-full h-12 bg-richBlack rounded-xl mt-4 active:scale-95 transition-all">
+    <p className="text-[16px] text-white font-medium">Create account</p>
+  </button>
+);
+
+const SocialLoginOptions = () => (
+  <>
+    <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-4 h-[1px] w-full"></div>
+    <div className="flex flex-col gap-2">
+      <SocialButton icon={<FcGoogle size={20} />} text="Continue with Google" />
+      <SocialButton icon={<FaFacebook size={20} className="text-blue-600" />} text="Continue with Facebook" />
+    </div>
+  </>
+);
+
+const SocialButton = ({ icon, text }: { icon: React.ReactNode; text: string }) => (
+  <button className="flex justify-center items-center w-full h-11 border border-gray-lightGray rounded-full gap-2">
+    {icon}
+    <p>{text}</p>
+  </button>
+);
+
+const LoginPrompt = () => (
+  <div className="flex justify-center items-center gap-2 mt-5">
+    <p className="text-gray-darkGray">Already have an account?</p>
+    <Link to={"/login"} className="font-semibold underline underline-offset-2">Log in</Link>
+  </div>
+);
+
+const FooterSection = () => (
+  <div className="w-full">
+    <Footer hasMiniHighlights={false} hasBorder={true} />
+  </div>
+);
 
 export default Signup;
